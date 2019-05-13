a4c74e0f13dced27bef9718d8cc4bc131afe43f3
Marvin <fuxxel66@gmail.com>
2019-05-13 20:04:09 +0200

    Tensorboard logging and pre training indexing of all training data

--- keras_coin.py
+++ keras_coin.py
@@ -1,12 +1,17 @@
 from keras.models import Model
-from keras.layers import Input, LSTM, Dense, CuDNNLSTM
+from keras.layers import Input, LSTM, Dense, CuDNNLSTM, Add
 from keras.utils import Sequence, to_categorical
+from keras.callbacks import TensorBoard
 from keras import backend as K
 
 import os
 import h5py
 import numpy as np
 import argparse
+import socket
+import random
+from datetime import datetime
+from collections import defaultdict
 
 class TrainingGenerator(Sequence):
 	def __init__(self, batch_size, clip_length, shrink, file_path, coin_class_override=None):
@@ -26,14 +31,26 @@ class TrainingGenerator(Sequence):
 
 		self.n_classes = len(self.coins)
 
+		self.indexes = defaultdict(list)
+		self.num_training_examples = 0
+
+		for coin in self.coins:
+			for gain in ["g8", "g16"]:
+				example_indexes = list(map(lambda x: (x, gain), list(self.data_file[str(coin)][gain].keys())))
+				random.shuffle(example_indexes)
+				self.indexes[int(coin)] = example_indexes
+
+				self.num_training_examples += len(example_indexes)
+
+		print("Indexed {} training examples".format(self.num_training_examples))
+
 	def get_random_example(self):
-		coin_choice = np.random.choice(self.coins, 1)[0]
-		gain_choice = np.random.choice(["g8", "g16"], 1)[0]
+		coin_choice = np.random.choice(list(self.indexes.keys()), 1)[0]
 
-		possible_examples = list(self.data_file[str(coin_choice)][gain_choice].keys())
-		example_choice = np.random.choice(possible_examples, 1)[0]
+		possible_examples = self.indexes[coin_choice]
+		example_choice = random.sample(possible_examples, 1)[0]
 
-		example_data = self.data_file[str(coin_choice)][gain_choice][example_choice]["values"][:][:self.clip_length:self.shrink]
+		example_data = self.data_file[str(coin_choice)][example_choice[1]][example_choice[0]]["values"][:][:self.clip_length:self.shrink]
 
 		example_max = np.max(example_data)
 		exmaple_min = np.min(example_data)
@@ -59,7 +76,7 @@ class TrainingGenerator(Sequence):
 		return [input_result_batch, teacher_result_batch], {"decoder_out" : output_result_batch, "classification_out": classification_result_batch}
 
 	def __len__(self):
-		return 200
+		return self.num_training_examples // self.batch_size
 
 
 def generate_model(hidden_dim, feature_size, n_classes):
@@ -91,12 +108,14 @@ def generate_model(hidden_dim, feature_size, n_classes):
 	decoder_dense = Dense(feature_size, activation='sigmoid', name="decoder_out")
 	decoder_outputs = decoder_dense(decoder_outputs)
 
-	classificator_fc = Dense(n_classes, activation="softmax", name="classification_out")
-	classificator = classificator_fc(state_h)
+	classificator_fc_h_1 = Dense(hidden_dim, activation="sigmoid")(state_h)
+	classificator_fc_c_1 = Dense(hidden_dim, activation="sigmoid")(state_c)
+	classificator_combine = Add()([classificator_fc_h_1, classificator_fc_c_1])
+	classificator_fc_out = Dense(n_classes, activation="softmax", name="classification_out")(classificator_combine)
 
 	# Define the model that will turn
 	# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-	model = Model(inputs=[encoder_inputs, decoder_inputs], outputs=[decoder_outputs, classificator])
+	model = Model(inputs=[encoder_inputs, decoder_inputs], outputs=[decoder_outputs, classificator_fc_out])
 
 	losses = {
 		"classification_out": "categorical_crossentropy",
@@ -114,12 +133,29 @@ def generate_model(hidden_dim, feature_size, n_classes):
 
 	return model
 
+def get_comment_string(args):
+	comment = "::d_" #if args.debug else ""
+	comment += "b{}_".format(args.batch_size)
+	comment += "hs{}_".format(args.hidden_size)
+	comment += "lstm_" #if args.use_lstm else "gru_"
+	comment += "s{}_".format(args.shrink)
+	comment += "e{}".format(args.epochs)
+	return comment
+
+def get_log_dir(comment):
+    current_time = datetime.now().strftime('%b%d_%H-%M-%S')
+    log_dir = os.path.join('runs', current_time + '_' + socket.gethostname() + comment)
+    return str(log_dir)
+
 def main(args):
 	model = generate_model(args.hidden_size, 1, n_classes=2)
 
 	training_generator = TrainingGenerator(batch_size=args.batch_size, clip_length=700000, shrink=args.shrink, file_path=args.file_path, coin_class_override=[5, 100])
 
-	model.fit_generator(training_generator, steps_per_epoch=200, epochs=args.epochs, shuffle=True)
+	callbacks = []
+	callbacks += [TensorBoard(log_dir=get_log_dir(get_comment_string(args)), batch_size=args.batch_size)]
+
+	model.fit_generator(training_generator, steps_per_epoch=len(training_generator), epochs=args.epochs, shuffle=True, callbacks=callbacks)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
