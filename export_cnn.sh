5d4e0775e4d0dcf24c7d9483d7900c9ce0996cb0
Marvin <fuxxel66@gmail.com>
2019-08-03 23:05:52 +0200

    Created new default exports for different methods

--- export_cnn.sh
+++ export_cnn.sh
@@ -3,7 +3,7 @@
 export MODE=train
 export NUM_EPOCHS=200
 export BATCH_SIZE=96
-export SAVE_PATH=trained_net
+export SAVE_PATH=results/weights/cnn
 export TOP_DB=2
 export HIDDEN_SIZE=100
 export FC_HIDDEN_SIZE=100
@@ -11,4 +11,4 @@ export SHRINK=16
 export WINDOW_SIZE=1024
 export WINDOW_GAP=1024
 export LEARNING_RATE=0.002
-export EXTRA_ARGS="-a enc_dec"
+export EXTRA_ARGS="-a cnn --log_dir results/stats/cnn"
